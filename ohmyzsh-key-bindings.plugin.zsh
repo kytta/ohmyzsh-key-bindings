3641782cf8995adb6bf7cee30afd9fa6ca4d0ec5
Nikita Karamov <nick@karamoff.dev>
2020-05-03 12:54:56 +0200

    Add initial lib code

---
+++ ohmyzsh-key-bindings.plugin.zsh
@@ -0,0 +1,9 @@
+#!/bin/zsh
+
+# ohmyzsh-key-bindings
+# https://github.com/NickKaramoff/ohmyzsh-key-bindings
+#
+# Initial source:
+# https://github.com/ohmyzsh/ohmyzsh/blob/95aa9bd97b9fb2a57f355178b2c5f01eb72ad0b6/lib/key-bindings.zsh
+
+source "${0:A:h}/ohmyzsh-key-bindings.zsh"
