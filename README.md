49c71b8c5f8003a6506391bc54de9e186b542e6c
Nikita Karamov <nick@karamoff.dev>
2020-05-03 13:47:03 +0200

    Add better documentation to tweaks

--- README.md
+++ README.md
@@ -8,10 +8,26 @@ with no bundler at all.
 
 **Features:**
 
-* history fuzzy finder
-* go to beginning/end of line using `Home`/`End`
-* move through command word-by-word using `Control-Right`/`Control-Left`
-* some other tweaks _(TODO: finish README)_
+* <kbd>PageUp</kbd>/<kbd>PageDown</kbd>  
+  navigate history up/down line-by-line
+* <kbd>↑</kbd>/<kbd>↓</kbd>  
+  fuzzy find input line in history
+* <kbd>Control</kbd>+<kbd>r</kbd>  
+  search history backwards incrementally
+* <kbd>Home</kbd>/<kbd>End</kbd>  
+  go to beginning/end of input line
+* <kbd>Control</kbd>+<kbd>←</kbd> / <kbd>Control</kbd>+<kbd>→</kbd>  
+  move cursor one word forward/backward
+* <kbd>Shift</kbd>+<kbd>Tab</kbd>  
+  navigate completion menu backwards
+* <kbd>Control</kbd>+<kbd>x</kbd>, then <kbd>Control</kbd>+<kbd>e</kbd>  
+  edit the currently input line in `$EDITOR`
+* <kbd>Esc</kbd>, then <kbd>w</kbd>  
+  erase from cursor to the beginning of the line
+* <kbd>Esc</kbd>, then <kbd>l</kbd>  
+  run `ls`
+* <kbd>Esc</kbd>, then <kbd>m</kbd>  
+  copy previous word (helpful for file renaming)
 
 ## License
 
--- ohmyzsh-key-bindings.zsh
+++ ohmyzsh-key-bindings.zsh
@@ -1,7 +1,9 @@
 #!/bin/zsh
 
-# Source: https://github.com/ohmyzsh/ohmyzsh/blob/95aa9bd97b9fb2a57f355178b2c5f01eb72ad0b6/lib/key-bindings.zsh
+# Source:
+# https://github.com/ohmyzsh/ohmyzsh/blob/95aa9bd97b9fb2a57f355178b2c5f01eb72ad0b6/lib/key-bindings.zsh
 
+# See more:
 # http://zsh.sourceforge.net/Doc/Release/Zsh-Line-Editor.html
 # http://zsh.sourceforge.net/Doc/Release/Zsh-Line-Editor.html#Zle-Builtins
 # http://zsh.sourceforge.net/Doc/Release/Zsh-Line-Editor.html#Standard-Widgets
@@ -19,62 +21,86 @@ if (( ${+terminfo[smkx]} )) && (( ${+terminfo[rmkx]} )); then
   zle -N zle-line-finish
 fi
 
-bindkey -e                                            # Use emacs key bindings
 
-bindkey '\ew' kill-region                             # [Esc-w] - Kill from the cursor to the mark
-bindkey -s '\el' 'ls\n'                               # [Esc-l] - run command: ls
-bindkey '^r' history-incremental-search-backward      # [Ctrl-r] - Search backward incrementally for a specified string. The string may begin with ^ to anchor the search to the beginning of the line.
+# Use Emacs' key bindings
+bindkey -e
+
+# [Esc] [w] - remove text from the cursor to the beginning of command
+bindkey '\ew' kill-region
+
+# [Esc] [l] - run `ls`
+bindkey -s '\el' 'ls\n'
+
+# [Ctrl]-[r] - search history backwards incrementally for an input string.
+#              The string may begin with ^ to anchor the search to the beginning
+#              of the line.
+bindkey '^r' history-incremental-search-backward
+
+# Navigate history with [PageUp]/[PageDown]
 if [[ "${terminfo[kpp]}" != "" ]]; then
-  bindkey "${terminfo[kpp]}" up-line-or-history       # [PageUp] - Up a line of history
+  # [PageUp] - Up a line of history
+  bindkey "${terminfo[kpp]}" up-line-or-history
 fi
 if [[ "${terminfo[knp]}" != "" ]]; then
-  bindkey "${terminfo[knp]}" down-line-or-history     # [PageDown] - Down a line of history
+  # [PageDown] - Down a line of history
+  bindkey "${terminfo[knp]}" down-line-or-history
 fi
 
-# start typing + [Up-Arrow] - fuzzy find history forward
+# Fuzzy find in history with [UpArrow]/[DownArrow] for an input string
 if [[ "${terminfo[kcuu1]}" != "" ]]; then
+  # start typing + [UpArrow] to fuzzy find in history forwards
   autoload -U up-line-or-beginning-search
   zle -N up-line-or-beginning-search
   bindkey "${terminfo[kcuu1]}" up-line-or-beginning-search
 fi
-# start typing + [Down-Arrow] - fuzzy find history backward
 if [[ "${terminfo[kcud1]}" != "" ]]; then
+  # start typing + [DownArrow] to fuzzy find in history backwards
   autoload -U down-line-or-beginning-search
   zle -N down-line-or-beginning-search
   bindkey "${terminfo[kcud1]}" down-line-or-beginning-search
 fi
 
+# Navigate line with [Home]/[End]
 if [[ "${terminfo[khome]}" != "" ]]; then
-  bindkey "${terminfo[khome]}" beginning-of-line      # [Home] - Go to beginning of line
+  # [Home] - go to beginning of line
+  bindkey "${terminfo[khome]}" beginning-of-line
 fi
 if [[ "${terminfo[kend]}" != "" ]]; then
-  bindkey "${terminfo[kend]}"  end-of-line            # [End] - Go to end of line
+  # [End] - go to end of line
+  bindkey "${terminfo[kend]}"  end-of-line
 fi
 
-bindkey ' ' magic-space                               # [Space] - do history expansion
+# [Space] - do history expansion
+bindkey ' ' magic-space
 
-bindkey '^[[1;5C' forward-word                        # [Ctrl-RightArrow] - move forward one word
-bindkey '^[[1;5D' backward-word                       # [Ctrl-LeftArrow] - move backward one word
+# [Ctrl-RightArrow] - move forward one word
+bindkey '^[[1;5C' forward-word
+# [Ctrl-LeftArrow] - move backward one word
+bindkey '^[[1;5D' backward-word
 
+# [Shift-Tab] - move through the completion menu backwards
 if [[ "${terminfo[kcbt]}" != "" ]]; then
-  bindkey "${terminfo[kcbt]}" reverse-menu-complete   # [Shift-Tab] - move through the completion menu backwards
+  bindkey "${terminfo[kcbt]}" reverse-menu-complete
 fi
 
-bindkey '^?' backward-delete-char                     # [Backspace] - delete backward
+# [Backspace] - delete char backwards (as you'd expect)
+bindkey '^?' backward-delete-char
+
+# [Delete] - delete char forwards
 if [[ "${terminfo[kdch1]}" != "" ]]; then
-  bindkey "${terminfo[kdch1]}" delete-char            # [Delete] - delete forward
+  bindkey "${terminfo[kdch1]}" delete-char
 else
   bindkey "^[[3~" delete-char
   bindkey "^[3;5~" delete-char
   bindkey "\e[3~" delete-char
 fi
 
-# Edit the current command line in $EDITOR
+# [Ctrl-x] [Ctrl-e] - Edit the currently input line in $EDITOR
 autoload -U edit-command-line
 zle -N edit-command-line
 bindkey '\C-x\C-e' edit-command-line
 
-# file rename magick
+# [Esc] [m] - copy previous word (helpful for file renaming)
 bindkey "^[m" copy-prev-shell-word
 
 # consider emacs keybindings:
